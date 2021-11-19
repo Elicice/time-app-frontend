5ef843a6068c083064e336ac42e6619fd0692bb2
Cecilie Lyshoel <cecilie@lyshoel.com>
2021-11-19 19:54:56 +0100

    Added an onClick

--- src/Overview.jsx
+++ src/Overview.jsx
@@ -8,42 +8,53 @@ import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import SettingsIcon from '@mui/icons-material/Settings';
 import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import style from './Overview.css'
+import { useState } from "react";
 
 
-function Overview(){
+function Overview() {
+
+    const [showText, setShowText] = useState(false);
+
+    var handleOnClick = () => {
+        setShowText(!showText);
+    }
+
     return (
-    <div>
-        <h1>
-            Overview
-        </h1>
-        <div className="settings">
-        <IconButton aria-label="Settings" size="large">
-            <SettingsIcon />
-        </IconButton>
-        </div>
         <div>
-            <BarChart />
+            <h1>
+                Overview
+            </h1>
+            <div className="settings">
+                <IconButton aria-label="Settings" size="large">
+                    <SettingsIcon />
+                </IconButton>
+            </div>
+            <div>
+                <BarChart />
+            </div>
+            <div className="">
+                <Stack spacing={2} direction="row">
+                    <Button variant="contained">This Week</Button>
+                    <Button variant="contained">This Month</Button>
+                    <Button variant="contained">This Year</Button>
+                    <Button variant="contained">Total</Button>
+                    <FormControlLabel control={<Checkbox defaultChecked />} label="Time spent" size="large" />
+                    <FormControlLabel control={<Checkbox />} label="Units" size="large" />
+                    <FormControlLabel control={<Checkbox />} label="Badges" size="large" />
+                </Stack>
+            </div>
+            {showText && <div>En tekst</div>}
+            <div>
+                <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                    <Fab color="primary" aria-label="add" onClick={handleOnClick}>
+                        <AddIcon />
+                    </Fab>
+                </Box>
+            </div>
         </div>
-        <div className="">
-            <Stack spacing={2} direction="row">
-                <Button variant="contained">This Week</Button>
-                <Button variant="contained">This Month</Button>
-                <Button variant="contained">This Year</Button>
-                <Button variant="contained">Total</Button>              
-                <FormControlLabel control={<Checkbox defaultChecked />} label="Time spent" size="large" />
-                <FormControlLabel control={<Checkbox />} label="Units" size="large" />
-                <FormControlLabel control={<Checkbox />} label="Badges" size="large" />                
-            </Stack>
-        </div>
-        <div>
-        <Box sx={{ '& > :not(style)': { m: 1 } }}>
-            <Fab color="primary" aria-label="add">
-                <AddIcon />
-            </Fab>
-        </Box>
-        </div>
-    </div>
     );
 }
 
